e48b92ca05e8310b7bfad2426c55e661cd1da28c
Vaibhav Rai <iamvaibhavrai@gmail.com>
2017-05-01 18:34:22 +0530

    Abhi and Ambi

--- Contest/April LOC 2017/Abhi And Ambi/abhi_and_ambi.py
+++ Contest/April LOC 2017/Abhi And Ambi/abhi_and_ambi.py
@@ -1,42 +1,9 @@
-res = []
-def calculateMex(l):
-    mex = 0
-    while True:
-        if mex in l:
-            mex += 1
-        else:
-            return mex
-
-def calculateGrundy(n):
-    global res
-    if n == 0:
-        return 0
-
-    l = []
-    for i in range(n):
-        if i+1 not in res:
-            print("i",i)
-            l.append(calculateGrundy(i))
-
-    return calculateMex(l)
-
-def main():
-    t = int(input())
-    global res
-    for i in range(t):
-        res = []
-        n,a = map(int,input().split(" "))
-        for j in range(n):
-            res.append(calculateGrundy(a))
-        xor = res[0]
-        for j in range(1,len(res)):
-            xor ^= res[j]
-        if xor == 0:
+for i in range(int(input())):
+    n,a = map(int,input().split(" "))
+    if a <= n:
+        if a == 2:
             print("Ambi")
-            print(res)
         else:
             print("Abhi")
-            print(res)
-
-if __name__ == '__main__':
-    main()
+    else:
+        print("Abhi")
