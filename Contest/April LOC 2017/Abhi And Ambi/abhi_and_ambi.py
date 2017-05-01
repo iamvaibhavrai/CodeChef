53a65c086b7d9ab1f3229a2c572763e45c87a7a1
Vaibhav Rai <iamvaibhavrai@gmail.com>
2017-05-01 18:40:41 +0530

    Abhi and Ambi

--- Contest/April LOC 2017/Abhi And Ambi/abhi_and_ambi.py
+++ Contest/April LOC 2017/Abhi And Ambi/abhi_and_ambi.py
@@ -1,6 +1,6 @@
 for i in range(int(input())):
     n,a = map(int,input().split(" "))
-    if a <= n and a%2 == 0:
-        print("Ambi")
-    else:
+    if a > n and a&1 == 0:
         print("Abhi")
+    else:
+        print("Ambi")
